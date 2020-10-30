83fa4c90d073e9266c0bf7e8811b8a294a533b86
pitcharath <73677783+pitcharath@users.noreply.github.com>
2020-10-30 10:43:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-?>
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>Welcome to my website</h1>
+        <p>My name is Pitchararth Phaiboontham</p>
+        <p>My name is Mek</p>
+        <p>First Picture</p>
+        <img src="Star.png">
+        <br>
+        <p>Second Picture</p>
+        <img src="Christmas_tree.jpg">
+        <br>
+        <a href="https://www.youtube.com">Favourite Website #1</a>
+        <a href="https://www.reddit.com">Favourite Website #2</a>
+        <p> Once when I was in high school. Me and my friend went to a school's shop.
+            I went to the snacks and sweets section and decided to brought up a pack of candies.
+            I showed the candies to my friend who is from across the shop. A second later, my
+            hand slipped and the candies fell and shattered on the floor.My friend who saw everything
+            laugh and so do I. After that, I have to bought the candies since I broke them.
+        </p>
+    </body>
+</html>
